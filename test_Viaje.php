597f625d614412cb05601b40432879cae17b6bd5
AgusDevelop <luciano.rojas@est.fi.uncoma.edu.ar>
2022-04-05 11:30:11 -0300

    Listo para entrega

--- test_Viaje.php
+++ test_Viaje.php
@@ -1,9 +1,6 @@
 <?php
 include "Viaje.php";
-$totales=[["nombre" => "Samanta", "apellido" => "Gomez", "documento" => 34505202],
-        ["nombre" => "Enzo", "apellido" => "Castro", "documento" => 31125810],
-        ["nombre" => "Kiara", "apellido" => "Luzbelito", "documento" => 28101250],
-        ["nombre" => "Oscar", "apellido" => "Bilardo", "documento" => 17310401]];
+$totales=[];
 
     //FUNCIONES
 
@@ -33,6 +30,9 @@ $totales=[["nombre" => "Samanta", "apellido" => "Gomez", "documento" => 34505202
     
     //PROGRAMA TEST
             
+     
+
+    echo " CARGAR NUEVO VIAJE \n";
     echo "Ingrese Codigo de Viaje: \n";
     $codigo=strtoupper(trim(fgets(STDIN)));
     echo "Ingrese Destino de Viaje: \n";
@@ -40,67 +40,64 @@ $totales=[["nombre" => "Samanta", "apellido" => "Gomez", "documento" => 34505202
     echo "El servicio es CAMA O SEMI?: \n";
     $tipoServicio=strtoupper(trim(fgets(STDIN)));
     $cantMaxPasajeros= cantidadMaxima($tipoServicio);
-    $objViaje = new Viaje($codigo,$destino,$cantMaxPasajeros,$totales); 
-        
+    $objViaje = new Viaje($codigo,$destino,$cantMaxPasajeros,$totales);  
+  
 
+  
+while ($opcion <> 6){
     do {
         echo "Menú de opciones Sistema Viaje Feliz: \n";
 		echo "   1)   Cargar Nuevo Pasajero \n";
         echo "   2)   Modificar Codigo Viaje \n";
 		echo "   3)   Modificar Destino Viaje \n";
-		echo "   4)   Modificar Datos Pasajeros \n";
-        echo "   5)   Ver datos Viaje \n";
+        echo "   4)   Ver datos Viaje \n";
+        echo "   5)   Ver datos Pasajeros del Viaje \n";
+        echo "   6)   SALIR \n";
 		echo "Elija una opción del menú: ";
 		$opcion = trim(fgets(STDIN));
-} while ($opcion < 1 || $opcion > 5);
+    } while ($opcion < 1 || $opcion > 7);
+
+       
 
     if($opcion==1){
         echo "Ingrese Nombre de Pasajero: \n";
-            $nombre=strtoupper(trim(fgets(STDIN)));
+        $nombre=strtoupper(trim(fgets(STDIN)));
         echo "Ingrese Apellido de Pasajero: \n";
-            $apellido=strtoupper(trim(fgets(STDIN)));
+        $apellido=strtoupper(trim(fgets(STDIN)));
         echo "Ingrese Documento de Pasajero: \n";
-            $dni=strtoupper(trim(fgets(STDIN)));
+        $dni=strtoupper(trim(fgets(STDIN)));
         $nuevoPasajero = $objViaje -> nuevoPasajero($nombre,$apellido,$dni);
         $objViaje -> sumarPasajero($nuevoPasajero);
 
     }elseif($opcion==2){
-        echo "Ingrese Codigo a Modificar: \n";
+        echo "Ingrese Nuevo Codigo : \n";
         $codigoModif = trim(fgets(STDIN));
         $objViaje -> setcodigo($codigoModif);
 
     }elseif($opcion==3){
-        echo "Ingrese Destino a Modificar: \n";
+        echo "Ingrese Destino Nuevo : \n";
         $destinoModif = trim(fgets(STDIN));
         $objViaje -> setdestino($destinoModif);
         
     }elseif($opcion==4){
-        echo "Ingrese datos de Pasajero a Modificar: \n ";
-        echo "Ingrese Nombre: \n";
-            $nombre=strtoupper(trim(fgets(STDIN)));
-        echo "Ingrese Apellido: \n";
-            $apellido=strtoupper(trim(fgets(STDIN)));
-        echo "Ingrese Documento: \n";
-            $dni=strtoupper(trim(fgets(STDIN))); 
-        $datosViejos = ["nombre" => $nombre, "apellido" => $apellido, "documento" => $dni];
-        echo "Ingrese datos nuevos del Pasajero: \n ";
-        echo "Ingrese Nombre: \n";
-            $nombreNuevo=strtoupper(trim(fgets(STDIN)));
-        echo "Ingrese Apellido: \n";
-            $apellidoNuevo=strtoupper(trim(fgets(STDIN)));
-        echo "Ingrese Documento: \n";
-            $dniNuevo=strtoupper(trim(fgets(STDIN))); 
-        $datosNuevos = ["nombre" => $nombreNuevo, "apellido" => $apellidoNuevo, "documento" => $dniNuevo];
+        echo $objViaje;
+        
 
-        $objViaje -> pasajeroModificar($datosViejos,$datosNuevos);
+        
 
     
     }elseif($opcion==5){
-        echo $objViaje;
-        echo "Pasajeros del Viaje: \n" .
-        print_r($objViaje->gettotalPasajeros()); "\n";
+        print_r($objViaje -> gettotalPasajeros());
+        
+        
     }
+        
 
+    
+
+
+}
+    
 
    
 
